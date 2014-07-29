8188c5fa99ad1602ce39a19503f1315847c4057d
Nick Van Hoogenstyn <nickvanhoog@gmail.com>
2014-07-29 14:46:53 -0700

    indent change

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 	var colors = ['#eefbff']
 
 	$('.cover_img').hover(function() 
-{		$(this).css('border', '5px solid #c8d6e4');
+	{
+		$(this).css('border', '5px solid #c8d6e4');
 	});
 
 	$('.cover_img').mouseout(function() {
